5e1fa6f478ddae157bc422af1abe2dc19139641d
Casie Siekman <casie.siekman@gmail.com>
2018-12-03 20:07:51 -0600

    update styling

---
+++ GSRVwebparts/src/webparts/bDayAnniversary/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBDayAnniversaryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BDayAnniversaryWebPartStrings' {
+  const strings: IBDayAnniversaryWebPartStrings;
+  export = strings;
+}
